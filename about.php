ca3ea24cb820e005b772e92cdc7f3e9d82ff52e4
kaleb-316 <kaleb.btc1@gmail.com>
2020-01-19 01:19:55 +0300

    about

---
+++ about.php
@@ -0,0 +1,126 @@
+<?php
+    session_start();
+   
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/lifestyleStore.png" />
+        <title>ETHIOPIAN ENIWOK</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <!-- jquery library -->
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+        <!-- Latest compiled and minified javascript -->
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- External CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div>
+            <?php
+                require 'header.php';
+            ?>
+            <div class="container">
+                    <h1>about</h1>		  
+		 <div class="col-md-4 about-grids">
+			 <h4>Our Vision</h4>
+			 <p>Vision of the system is make Ethiopia one of the top five tourist destinations of Africa in 2019 through the development of its cultural wealth and natural attraction</p>
+		 </div>
+		 <div class="col-md-4 about-grids grid2">
+			 <h4>Our Mission</h4>
+			 <p>This system established that specializes in providing a wide range of fascinating historical place around Ethiopia. Our clear mission is to serve by providing quality web-based that address their travel need.</p>
+		 </div>
+		 
+            </div>
+        </div></body>
+        <div class="container">
+                <div class="row">
+                    <div class="col-xs-2 ">
+                        <div class="thumbnail">
+                            <a href="#">
+                                <img src="img/Nechisar_National_Park_01.jpg" alt="Sony DSLR">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>Linda ashebir</h3>
+                                    <p></p>
+                                 
+                                    
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-xs-2 ">
+                        <div class="thumbnail">
+                            <a href="#">
+                                <img src="img/Nechisar_National_Park_01.jpg" alt="Sony DSLR">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>iskedar lekisa</h3>
+                                    <p></p>
+                                  
+                                    
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-xs-2 ">
+                        <div class="thumbnail">
+                            <a href="#">
+                                <img src="img/Nechisar_National_Park_01.jpg" alt="Sony DSLR">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>Chalie Lijalem</h3>
+                                    <p></p>
+                                   
+                                    
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-xs-2 ">
+                        <div class="thumbnail">
+                            <a href="#">
+                                <img src="img/Nechisar_National_Park_01.jpg" alt="Sony DSLR">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>elias mekuanent</h3>
+                                    <p></p>
+                                   
+                                    
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                                        <div class="col-xs-2 ">
+                        <div class="thumbnail">
+                            <a href="#">
+                                <img src="img/Nechisar_National_Park_01.jpg" alt="Sony DSLR">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>kaleb tesfaye</h3>
+                                    <p></p>
+                                </div>
+                            </center>
+                        </div>
+                    </div> 
+                    </div>
+            <br><br> <br><br><br><br>
+           <footer class="footer"> 
+               <div class="container">
+               <center>
+                   <p>Copyright &copy. All Rights Reserved.</p>
+                   <p>This website is developed by ASTU CSE STUDENTS</p>
+               </center>
+               </div>
+           </footer>
+                    </div>
+                
+</html>
