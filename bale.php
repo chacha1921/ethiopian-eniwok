53618c397413efb0921d62eb19b67d12664a0886
newyearli <lindaholy21@gmail.com>
2020-01-21 11:34:40 +0300

    interface shoes about bale mountain park

---
+++ bale.php
@@ -0,0 +1,101 @@
+<?php
+    session_start();
+   
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/lifestyleStore.png" />
+        <title>ETHIOPIAN ENIWOK</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <!-- jquery library -->
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+        <!-- Latest compiled and minified javascript -->
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- External CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div>
+            <?php
+                require 'header.php';
+            ?>
+            <div class="container">
+                <div class="jumbotron">
+                    <h1>Welcome to Bale Mountain National park!</h1>
+                    
+                     <a href="https://www.weather-forecast.com/locations/Goba/forecasts/latest">Latest Weather</a>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="thumbnail">
+                            <a href="#">
+                                <img src="img/Bale_Mountns.jpg">
+                            </a>
+                        </div>
+                               </div>
+                               <div class="col-md-3">
+                        <div class="thumbnail">
+                            <a href="#">
+                                <img src="img/lion.jpg" alt="Cannon">
+                            </a>
+                        </div>
+                               </div>
+                               <div class="col-md-3">
+                        <div class="thumbnail">
+                            <a href="#">
+                                <img src="img/green.jpg" alt="Cannon">
+                            </a>
+                        </div>
+                               </div>
+                               <div class="col-md-3">
+                        <div class="thumbnail">
+                            <a href="#">
+                                <img src="img/bale2.jpg" alt="Cannon">
+                            </a>
+                        </div>
+                               </div>
+                    
+                            <center>
+                                <div class="caption">
+                                    <h3>Bale Mountain National Park</h3>
+                                    <p>The Bale Mountains National Park is located in southeastern Ethiopia, 400km southeast of Addis Ababa and 150km east of Shashamene in the Oromia Regional National State. It belongs to the Bale-Arsi massif, which forms the western section of the southeastern Ethiopian highlands.<br>
+
+Nominated in 2009 to UNESCO’s World Heritage Site Tentative List, the Bale Mountains are Ethiopia’s best-kept secret. Dominating the southeastern edge of the Rift Valley highlands, these mountains encompass the largest expanse of Afroalpine habitat in the world. It has been estimated that if conservation efforts in the Bale Mountains are not successful and people continue to exploit the resources in an unsustainable way, more species of mammal would become extinct than in any other area of equivalent size on the planet.<br>
+
+Ethiopia is one of the top 25 biodiversity-rich countries in the world, and the Bale Mountains have one of the highest incidences of animal endemism of any terrestrial habitat on earth; the park itself holds 26 percent of Ethiopia’s endemic species. In addition to a wealth of endemic, unique and endangered mammals, Bale Mountains is home to a wide range of bird species. Rated by the African Bird Club as the number four birding spot in Africa, the Bale Mountains are home to over 300 species of birds – six of which are endemic to Ethiopia and 11 to Ethiopia and neighboring Eritrea.<br>
+
+The Bale Mountains are an area of supreme importance for numerous reasons. They sustain the life of many endemic and endangered species. Additionally, over 12 million people and their livestock – in the southeast region of Ethiopia, northern Kenya and Somalia – rely on the water that originates from the Bale massif.<br>
+
+The Bale Mountains National Park offers opportunities for unsurpassed mountain walking, horse trekking, and scenic driving. It also presents the chance to view many of the endemic mammals that reside within the park, specifically the mountain nyala and Ethiopian wolf, as well as birds such as the wattled crane and bearded vulture.<br>
+
+However, perhaps the primary attraction of the Bale Mountains is the simple escape it offers from city life; into peace, serenity and beauty.<br>
+                                </div>
+                            </center>
+                         </div>
+                            <center>
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1980.5199487157927!2d39.733243655664715!3d6.885824620756229!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x17b638c226fa3a17%3A0x398e4e107fc41cec!2sBale%20Mountains%20National%20Park!5e0!3m2!1sen!2set!4v1576962124931!5m2!1sen!2set" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                    </center>
+                   
+                    </div>
+                    <br><br><br><br><br><br><br><br>
+           <footer class="footer">
+               <div class="container">
+                <center>
+                   <p>Copyright &copy . All Rights Reserved.</p>
+                   <p>This website is developed by <em>Astu CSE Students</em></p>
+               </center>
+               </div>
+           </footer>
+                    </div>
+                   
+                   
+         
+    
+    </body>
+</html>
