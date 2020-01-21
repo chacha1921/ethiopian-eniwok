97979cbff18134861377b3a84e827e26d7858444
newyearli <lindaholy21@gmail.com>
2020-01-21 11:50:33 +0300

    information about the  park

---
+++ nechsar.php
@@ -0,0 +1,104 @@
+<?php
+    session_start();
+   
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/lifestyleStore.png" />
+        <title>ETHIOPIAN ENIWOK</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <!-- jquery library -->
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+        <!-- Latest compiled and minified javascript -->
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- External CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div>
+            <?php
+                require 'header.php';
+            ?>
+            <div class="container">
+                <div class="jumbotron">
+                    <h1>Welcome to NechSar National park!</h1>
+                     <a href="https://www.weather-forecast.com/locations/Arba-Minch/forecasts/latest">Latest Weather</a>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="thumbnail">
+                            <a href="#">
+                                <img src="img/nech1.jpg">
+                            </a>
+                        </div>
+                               </div>
+                               <div class="col-md-3">
+                        <div class="thumbnail">
+                            <a href="#">
+                                <img src="img/nech2.jpg">
+                            </a>
+                        </div>
+                               </div>
+                               <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="#">
+                                <img src="img/zebra.jpg">
+                            </a>
+                        </div>
+                               </div>
+                               <div class="col-md-3">
+                        <div class="thumbnail">
+                            <a href="#">
+                                <img src="img/Nechisar_National_Park_01.jpg">
+                            </a>
+                        </div>
+                               </div>
+                    
+                </div>
+                                <div class="caption">
+                                    <center>
+                                    <h3>NechSar National Park</h3>
+                                        </center>
+                                    <p>
+Nechisar National Park is situated 510km south of Addis near the town of Arba Minch, in between Lakes Abaya and Chamo. From the town on the ridge of land that divides Abaya and Chamo there are commanding panoramic views all around, including both lakes with Nechisar on the eastern side and, to the west, the Guge range of mountains. The outstanding beauty of the neck of land between the two lakes has earned it the sobriquet of'Bridge of Heaven'. The equally poetic Arba Minch - meaning 'forty springs' - takes its name from the bubbling streams which spring up amid the undergrowth of the luxuriant groundwater forest that covers the flats beneath the town. This alluring area is considered one of Ethiopia's last great surviving wildernesses.<br> <br> 
+
+Nechisar National ParkA wide variety of plains game roam freely amongst 514 km2 of savannah, dry bush and ground water forest, which are also the habitat of 188 recorded species of birds. Animals to be seen are Bushbuck, Swayne's Hartebeest, Burchell's Zebra, Grant's Gazelle, Guenther's Dik-dik, Greater Kudu, Crocodile, Anubis Baboon, Grey Duiker. Birds seen include Red-billed Hornbill, Grey Hornbil,l Fish Eagle, Kori Bustard, Abyssinian Ground Hornbill. A backdrop of hills and mountains combine to make this one of the most attractive national parks in Ethiopia, and its location makes Nechisar Park very accessible. In the far eastern part of the Nechisar park is hot springs bubble to the surface.The shores and islands of Abaya and Chamo are populated by farming peoples such as the Ganjule and the Guji, both of whom also have ancient traditions of hippo hunting. The Guji ply the Lake Abaya waters in elegantly curved high-prowed ambatch boats similar to those depicted on the tombs of Egyptian phar-aohs. Made of extremely light wood, an ambatch is capable of transporting several cattle at one time and is sufficiently sturdy to withstand any attack by crocodiles, which are present in large numbers - and large sizes - on both lakes.<br> <br> 
+
+Nechisar National ParkNechisar National Park<br> <br> 
+The vivid contrasts of the Nechisar National Park will linger long in your memory - a swathe of white grass against the backdrop of clearly defined, deeply cut hills and mountains. From the escarpment on which Arba Minch stands you look down on the clear blue waters of Lake Chamo and the sandy beaches of its northern shores, covered by crocodiles lounging in the sun.<br> <br> 
+
+To the north of Nechisar National Park, Lake Abaya's surface is a startling contrast of dark red, caused by the suspended load of ferrous hydroxide in its waters. At the base of the escarpment is a large area of groundwater forest around the Kulfo River, as well as the 'forty springs' after which Arba Minch is named. The western edge of the Rift Valley forms an impressive backdrop to the west. Within the forest are shy, chestnut-red bushbuck, the comical bushpig, troops of Anubis baboons, and vervet monkeys.<br> <br> 
+
+Nechisar National ParkThe most commonly seen creatures of Nechisar’s bush and savannah are two extremes of antelope: the large greater kudu, with its spectacular spiral horns and white-striped flanks, and the minuscule Guenther's dik-dik.<br> <br> 
+
+At first sight the Nechisar plains, which you encounter as you leave the peninsula between the two lakes, seem surprisingly empty. But dotting this apparently endless sweep of golden white grass are herds of Burchell's zebra, which mingle with Grant's gazelle and an occasional Swayne's hartebeest, an endemic subspecies. Also seen are black-backed jackal and African hunting dog.<br> <br> 
+
+The many and varied bird species reflect the different habitats within the Nechisar park. As well as their crocodiles and bird life, lakes Abaya and Chamo are famous for their sport fishing potential, especially for Nile perch - often weighing more than 100 kilos (220 pounds) - and for the fighting 'tiger fish'.      <br>  <br>               
+                                </div>
+                            <center>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3967.77207748113!2d37.56572661444841!3d6.025991930419276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x17babc96ec793a1b%3A0x35422b2c5ef1ddab!2sNechisar%20National%20Park%20HQ!5e0!3m2!1sen!2set!4v1576962905715!5m2!1sen!2set" width="600" height="450" frameborder="0" style="border:10;" allowfullscreen=""></iframe>
+                                </center>
+                        
+                    </div>
+                    <br><br><br><br><br><br><br><br>
+           <footer class="footer">
+               <div class="container">
+                <center>
+                   <p>Copyright &copy . All Rights Reserved.</p>
+                   <p>This website is developed by <em>Astu CSE Students</em></p>
+               </center>
+               </div>
+           </footer>
+                    </div>
+                   
+                   
+         
+    
+    </body>
+</html>
