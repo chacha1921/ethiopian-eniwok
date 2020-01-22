a12feeb663a1480fcf13ecc34f4c1cf7740966a3
elu-astu <eliasnmnm@gmail.com>
2020-01-22 01:32:44 -0800

    Elias branch

---
+++ timiket.php
@@ -0,0 +1,65 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/lifestyleStore.png" />
+        <title>Projectworlds Store</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- latest compiled and minified CSS -->
+       <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css"> 
+        <!-- jquery library -->
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+        <!-- Latest compiled and minified javascript -->
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- External CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div>
+           <?php
+           // require 'header.php';
+           ?>
+           <div class="container">
+               <div class="row">
+                   <h1>TIMKET - (Epiphany the baptism of Christ)</h1>
+                   <p>Timket is the greatest festival of the Ethiopian year, falling just 2 weeks after Ethiopian Christmas. It is actually a 3-day affair preceded by the eve of Timket when the dramatic processions take place through a night of fasting, to the great day itself and the commemoration of Christ's baptism in the Jordan River. Ketera, the Eve of Timket is when the Priests bring out the Tabots - replicas of the 2 tablets of laws received by Moses, which are normally housed inside the altar symbolizing the Ark of the Covenant. Priests bless the water of the pool or river where the next day's celebration will take place. It is the Tabot, rather than the church building which is consecrated and given extreme reverence. Visitors have the unique chance to experience a festival lost to the rest of the world.</p>
+                   <div class="col-xs-6">
+                       <div  class="thumbnail">
+                           <a href="#">
+                                <img src="img/timket.jpg" alt="Camera">
+                           </a>
+                           <center>
+                                <div class="caption">
+                                        <p id="autoResize">TIMKET</p>
+                            
+
+                                </div>
+                           </center>
+                       </div>
+                   </div>
+                   <div class="col-xs-6">
+                       <div class="thumbnail">
+                           <a #>
+                               <img src="img/images%20(17).jfif" alt="Watch">
+                           </a>
+                       </div>
+                   </div>
+               </div>
+               
+                   
+            <br><br> <br><br><br><br>
+           <footer class="footer"> 
+               <div class="container">
+               <center>
+                   <p>Copyright &copy . All Rights Reserved.</p>
+                   <p>This website is developed by <em>ASTU CSE STUDENTS</em></p>
+               </center>
+               </div>
+           </footer>
+        </div>
+        </div>
+    </body>
+</html>
