7a6b056bb6d76d319a54661994c6a645ab22c871
elu-astu <eliasnmnm@gmail.com>
2020-01-22 01:15:16 -0800

    world heritages

---
+++ harar.php
@@ -0,0 +1,109 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/lifestyleStore.png" />
+        <title>Projectworlds Store</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- latest compiled and minified CSS -->
+       <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css"> 
+        <!-- jquery library -->
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+        <!-- Latest compiled and minified javascript -->
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- External CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div>
+           <?php
+            require 'header.php';
+           ?>
+           <div class="container">
+               <div class="row">
+                   <h1>Harar Jugol, the Fortified Historic Town</h1>
+                   <p>The fortified historic town of Harar is located in the eastern part of the country on a plateau with deep gorges surrounded by deserts and savannah. The walls surrounding this sacred Muslim city were built between the 13th and 16th centuries. Harar Jugol, said to be the fourth holiest city of Islam, numbers 82 mosques, three of which date from the 10th century, and 102 shrines, but the townhouses with their exceptional interior design constitute the most spectacular part of Harar's cultural heritage. The impact of African and Islamic traditions on the development of the town's building types and urban layout make for its particular character and uniqueness.</p>
+                   <div class="col-xs-12">
+                       <div  class="thumbnail">
+                           <a href="products.php">
+                                <img src="img/harar.jpg" alt="Camera">
+                           </a>
+                           <center>
+                                <div class="caption">
+                                        <p id="autoResize">Harar Jugol</p>
+                                    <h2>Outstanding Universal Value</h2>
+                                    <h3><b>Brief Synthesis</b></h3>
+                                        <p>
+
+The fortified historic town of Harar is located in the eastern part of Ethiopia, 525 km from the capital of Addis Ababa, on a plateau with deep gorges surrounded by deserts and savannah. The walls surrounding this sacred city, considered “the fourth holy city” of Islam, were built between the 13th and 16th centuries and served as a protective barrier. There were five historic gates, which corresponded to the main roads to the town and also served to divide the city into five neighbourhoods, but this division is not functional anymore. The Harar gate, from where the main streets lead to the centre, is of recent construction.<br><br>
+
+Harar Jugol numbers 82 mosques, three of which date from the 10th century, 102 shrines and a number of traditional, Indian and combined townhouses with unique interior designs, which constitute a spectacular part of Harar's cultural heritage. The African and Islamic traditions influenced over a long period of time the development of the city and its typical urban planning and contributed to its particular character and uniqueness. The present urban layout follows the 16th century design for an Islamic town with its central core occupied with commercial and religious buildings and a maze of narrow alleyways with imposing facades. The traditional Harari house has a typical, specific and original architectural form, different from the domestic layout usually known in Muslim countries, although reminiscent of the coastal Arab architecture, and with an exceptional interior design. At the end of the 19thcentury Indian merchants built new houses with wooden verandas that defined a different urban landscape and influenced the construction of the combined Indian/Harari houses. Their architectural and ornamental qualities are now part of the Harari cultural heritage.<br><br>
+
+Harar functioned as the capital of the Harari Kingdom from 1520 to 1568, became an independent emirate in the 17th century and was integrated into Ethiopia in 1887. From the late 16th century to the 19th century Harar was an important trade centre between the coast and the interior highlands and a location for Islamic learning. <br><br>
+
+Today Harar is the administrative capital of the Harari People National Regional State (HPNRS). The historic town has a traditionally functioning community, forming a complex social-environmental whole where each element has its symbolic and practical significance. The Harari people are distinguished by the continued cultural traditions and quality of their handicrafts, including weaving, basket making and book binding. The organization of the communities through traditional systems has preserved its social and physical inheritance and, significantly, the Harari language.<br><br>
+<b>Criterion (i):</b>The historic town of Harar Jugol exhibits an important interchange of values of original Islamic culture, expressed in the social and cultural development of the city enclosed within the otherwise Christian region. Such influences have been merged with traditions that relate to the inland of Africa and particularly to southern Ethiopia, giving a particular characteristic form to its architecture and urban plan.
+
+<br><br>
+<b>Criterion (ii):</b> Harar Jugol bears exceptional testimony to cultural traditions related to Islamic and African roots. It is considered “the fourth holy city” of Islam, having been developed by a holy missionary from the Arabic Peninsula. Though a trading place and thus a melting pot of various influences, Harar has been in relative isolation in its region, contributing to a cultural specificity, expressed in its characteristic community structure and traditions, which are still alive.<br>
+<b>Criterion (iii):</b> Harar Jugol is an outstanding example of a type of architectural and urban ensemble which illustrates the impact of African and Islamic traditions on the development of specific building types. The building types and the entire urban layout reflect these traditions, which give a particular character and even uniqueness to Harar Jugol.<br>
+
+
+ <b>Criterion (iv):</b>Harar Jugol with its surrounding landscape is an outstanding example of a traditional human settlement, representative of cultural interaction with the environment. The social and spatial structure (afocha) and the language of the people all reflect a particular and even unique relationship that there developed with the environment. The cultural and physical relationships with the territory have survived till today, but they are also vulnerable to irreversible change under the impact of the modern globalizing world.<br><br>
+
+<b>Integrity</b><br><br>
+
+The inscribed property of Harar Jugol has a core zone of 48 ha which encompass the entire walled city and contains all the attributes that sustain the Outstanding Universal Value of the property. The buffer zone extends 800m to the south and 1700m to the east whilst, on the west side, it is narrow and confined by the new town of Harar. Urban encroachment, on the western edge of the walled town, is the current concern.<br><br>
+
+Although there have been some urban development towards the west and north parts, the historic city remains intact on the eastern and south-eastern part of the walled town where the essential relationship between the urban and rural areas is still maintained.<br><br>
+
+Except for some changes that took place in the 19th and 20th centuries, such as the replacement of the principal Mosque by the Orthodox Church, and the enlargement of the main street leading from the western gate, the historic city has kept its traditional housing reasonably intact.
+
+ <br><br>
+
+However, the integrity of the property can be threatened by emerging trends to alter and modernize the traditional buildings, which would make them susceptible to irreversible change. Careful monitoring, enforcement of regulations, raising awareness and the promotion of preservation attitudes amongst the inhabitants are actions needed to maintain integrity. <br><br>
+
+                                            <b>Authenticity</b><br><br>
+
+Harar Jugol is a rare example of a relatively well preserved historic town that has retained its traditions, urban fabric, and rich Harari Muslim cultural heritage to the present time. It is one of the holy towns of Islam in Africa, and the capital of a minority region within Christian Ethiopia. The historic city is physically limited and well defined by its 16th century surrounding wall and the setting has been retained along the eastern and south-eastern sides of the property. However, inappropriate interventions, such as plastering the houses, changing doors from wood to metal, the introduction of non-traditional materials and visual impacts such as TV antennas have been gradually affecting the authenticity of the historic fabric. Guidelines for interventions need to be enforced and communicated amongst the inhabitants to prevent further impacts on the authenticity of the property.<br><br>
+
+                                            <b>Protection and Management Requirements</b><br><br>
+
+Harar has been officially registered as an Ethiopian National Heritage site since 1974. The legislative framework which protects the property includes the “Heritage Conservation Draft Proclamation of Harari People National Regional State” (January 2000), "The Establishment of Harar Heritage Conservation Office" (Proclamation no. 21/1992) and the Federal Proclamation no. 209/2000 for “Research and Conservation of Cultural Heritage”. In addition, four levels of protection have been identified for the property within the Management Plan: principal monuments, important historic buildings, contextual urban fabric and ‘out-of-context’ buildings.<br><br>
+
+The Centre for Research and Conservation of Cultural Heritage (ARCCH), established in 1976, is responsible for the inventory and the definition of conservation policies, providing support for restoration work, and making decisions over grants and permits. The local authority and the Kebele act as administrative offices in the process. The Jugol Heritage Conservation Office (JHCO), established in 2003, has a management committee and serves as a liaison between the Harari Counsel, under the General Meeting of the Harari People National Regional State, and representatives of the administrative and social structure in Jugol. The main source of funding comes from the government. However, there has been cooperation between the local authority, the Urban Development Support Service, and the German Technical Organization.<br><br>
+
+The Urban Master Plan and the GIS system that inventories historic structures are tools to drive decision-making at the property. The Master Plan has the main objectives of preserving historic heritage, improving living conditions for the inhabitants and the promotion of tourism in addition to the conservation of the agricultural landscape in the buffer zone. Factors that need to be addressed through management and conservation actions include the enforcement of regulations for new constructions, infrastructure development, waste management, the maintenance and conservation of historic buildings as well as the preservation of the setting. The management of Harar Jugol will need to address the challenges faced to achieve the delicate balance between the need for conservation of cultural heritage and traditional values with those for the improvement of quality of life and sustainable development.</p>
+                                </div>
+                           </center>
+                       </div>
+                   </div>
+                   <div class="col-xs-12">
+                       <div class="thumbnail">
+                           <a href="products.php">
+                               <img src="img/harar2.jpg" alt="Watch">
+                           </a>
+                           
+                       </div>
+                   </div>
+                   <center>
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31497.838803799506!2d42.10063909834432!3d9.312890972899295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1631bda3c73a509d%3A0x47a0ad82e9e99ed6!2sHarar!5e0!3m2!1sen!2set!4v1576942437848!5m2!1sen!2set" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                           </center>
+               </div>
+                   
+            <br><br> <br><br><br><br>
+           <footer class="footer"> 
+               <div class="container">
+               <center>
+                   <p>Copyright &copy Store. All Rights Reserved.</p>
+                   <p>This website is developed by Yugesh Verma</p>
+               </center>
+               </div>
+           </footer>
+        </div>
+        </div>
+    </body>
+</html>
